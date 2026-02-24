13509d7e00169da404fee11cd624aefdddc2a86f
Варвара <barbara_littel@mail.ru>
2026-02-24 13:22:39 +0300

    Add Bitcoin read date file script

--- src/loader.py
+++ src/loader.py
@@ -1,1 +1,15 @@
-print("Loading data from https://www.kaggle.com/datasets/yamqwe/cryptocurrency-extra-data-bitcoin?resource=download")
+import pandas as pd
+import os
+
+def load_bitcoin_data():
+    path = 'data/full_data__1__2021.csv'
+    if os.path.exists(path):
+        # В учебных целях просто имитируем чтение
+        print(f"Загрузка данных из {path}...")
+        # df = pd.read_csv(path) 
+        print("Данные успешно загружены. Начинаем анализ волатильности.")
+    else:
+        print("Файл с данными не найден!")
+
+if __name__ == "__main__":
+    load_bitcoin_data()
